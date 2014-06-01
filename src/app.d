675082af01f2363252a61e9eb6731bfd8d67dea8
w0rp <devw0rp@gmail.com>
2014-06-01 14:18:00 +0100

    Add a download page and a bunch of stuff.

--- src/app.d
+++ src/app.d
@@ -2,9 +2,20 @@ import vibe.http.server;
 import vibe.http.fileserver;
 import vibe.appmain;
 
+alias Request = HTTPServerRequest;
+alias Response = HTTPServerResponse;
+
 template basicPage(string templatePath) {
-    void basicPage(HTTPServerRequest request, HTTPServerResponse response) {
+    void basicPage(Request request, Response response) {
         response.render!(templatePath, request);
+    }
+}
+
+// TODO: This uses an HTML style redirect.
+// This needs replacing with something else.
+template redirectPage(string toURL) {
+    void redirectPage(Request request, Response response) {
+        response.redirect(toURL, 301);
     }
 }
 
@@ -21,6 +32,9 @@ shared static this() {
 
     router
     .get("/", &basicPage!"index.dt")
+    // Old page URLs are 301 redirected to the new URLs.
+    .get("/download.html", &redirectPage!"/download")
+    .get("/download", &basicPage!"download.dt")
     .get("/static/*", serveStaticFiles("../static/", fileSettings))
     ;
 
