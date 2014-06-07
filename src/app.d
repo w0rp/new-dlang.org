1167f70581e0445f80a74b9a84d78a80c86b933a
w0rp <devw0rp@gmail.com>
2014-06-07 21:39:21 +0100

    Add changelogs from 2.000 to 2.052.

--- src/app.d
+++ src/app.d
@@ -1,17 +1,18 @@
 import std.algorithm;
 import std.array;
+import std.typetuple;
 
 import vibe.http.server;
 import vibe.http.fileserver;
 import vibe.appmain;
 
-template ArrayExpressionTuple(alias array) {
+template StaticArrayToExpressionTuple(alias array) {
     import std.typetuple;
 
     static if (array.length == 0) {
-        alias ArrayExpressionTuple = TypeTuple!();
+        alias StaticArrayToExpressionTuple = TypeTuple!();
     } else {
-        alias ArrayExpressionTuple = Impl!(array.length - 1);
+        alias StaticArrayToExpressionTuple = Impl!(array.length - 1);
 
         private template Impl(size_t index) {
             static if (index == 0) {
@@ -23,12 +24,41 @@ template ArrayExpressionTuple(alias array) {
     }
 }
 
+/**
+ * Convert a slice at compile time to a static array.
+ *
+ * Note: This doesn't account for a 0-size slice.
+ */
+template SliceToStaticArray(alias slice) {
+    enum typeof(slice[0])[slice.length] SliceToStaticArray = slice[0 .. $];
+}
+
+/**
+ * Convert a slice known at compile time to an expression tuple.
+ *
+ * Note: This doesn't account for a 0-size slice.
+ */
+template SliceToExpressionTuple(alias slice) {
+    enum SliceToExpressionTuple =
+        StaticArrayToExpressionTuple!(SliceToStaticArray!slice);
+}
+
 alias Request = HTTPServerRequest;
 alias Response = HTTPServerResponse;
 
 template basicPage(string templatePath) {
     void basicPage(Request request, Response response) {
         response.render!(templatePath, request);
+    }
+}
+
+template changelogPage(string thisVersion) {
+    void changelogPage(Request request, Response response) {
+        response.render!(
+            "changelog/" ~ thisVersion ~ ".dt",
+            request,
+            thisVersion
+        );
     }
 }
 
@@ -40,6 +70,65 @@ template redirectPage(string toURL) {
     }
 }
 
+// Load the list of basic view files we got from a pre-build step.
+enum basicFileTuple = SliceToExpressionTuple!(
+    import("basic_view_list").split("\n")
+);
+
+enum changelogTuple = TypeTuple!(
+    "2.000",
+    "2.001",
+    "2.002",
+    "2.003",
+    "2.004",
+    "2.005",
+    "2.006",
+    "2.007",
+    "2.008",
+    "2.009",
+    "2.010",
+    "2.011",
+    "2.012",
+    "2.013",
+    "2.014",
+    "2.015",
+    "2.016",
+    "2.017",
+    "2.018",
+    "2.019",
+    "2.020",
+    "2.021",
+    "2.022",
+    "2.023",
+    "2.025",
+    "2.026",
+    "2.027",
+    "2.028",
+    "2.029",
+    "2.030",
+    "2.031",
+    "2.032",
+    "2.033",
+    "2.034",
+    "2.035",
+    "2.036",
+    "2.037",
+    "2.038",
+    "2.039",
+    "2.040",
+    "2.041",
+    "2.042",
+    "2.043",
+    "2.044",
+    "2.045",
+    "2.046",
+    "2.047",
+    "2.048",
+    "2.049",
+    "2.050",
+    "2.051",
+    "2.052",
+);
 
 shared static this() {
     import vibe.http.router;
@@ -52,24 +141,20 @@ shared static this() {
 
     auto router = new URLRouter;
 
-    enum basicFileList = import("basic_view_list").split("\n");
-
-    enum string[basicFileList.length] staticList =
-        basicFileList[0 .. basicFileList.length];
-
-    enum basicFileTuple = ArrayExpressionTuple!staticList;
-
     foreach(dietFilename; basicFileTuple) {
+        // Set up routes with compiled templates for each file that we found.
         router.get(
             "/" ~ dietFilename["basic/".length .. $ - ".dt".length],
             &basicPage!dietFilename
         );
     }
 
+    foreach(thisVersion; changelogTuple) {
+        router.get("/changelog/" ~ thisVersion, &changelogPage!thisVersion);
+    }
+
     router
     .get("/", &basicPage!"index.dt")
-    //.get("/download", &basicPage!"download.dt")
-    //.get("/changelog", &basicPage!"changelog.dt")
     // Old page URLs are 301 redirected to the new URLs.
     .get("/download.html", &redirectPage!"/download")
     .get("/changelog.html", &redirectPage!"/changelog")
